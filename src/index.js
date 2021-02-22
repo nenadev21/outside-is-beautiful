ce2883c1f39f42cb141a5271b9e12c8e3d91ec74
Dani <69801600+nenadev21@users.noreply.github.com>
2021-02-21 20:40:05 -0500

    Added forecast and icons
    Added forecast for 6 following days. Information is presented through JS instead of HTML. Added call to get icons from api.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+//this creates a variable that detects the current date. This is provided by JS
 let now = new Date();
 
 // Function that populates document with current day and time
-function currentDayAndTime() {
+function currentDayAndTime(timestamp) {
+
   let daysIndex = [
     `Sunday`,
     `Monday`,
@@ -33,9 +35,9 @@ function currentDayAndTime() {
   return `${dd}, ${hh}:${min} ${meridiem}`;
 }
 
-// end of currentDayAndTime function
+// here's where the currentDayAndTime function ends
 
-// Here I'm calling the fuction and ordering it to present the info on the document.
+// Here I'm calling the fuction above and ordering it to present the info on the document.
 // If I don't call the fuction nothing will happen
 
 let dayAndTime = document.querySelector("#day-and-time");
@@ -64,6 +66,7 @@ function currentDateAndYear() {
 }
 
 // End of currentDateAndYear function
+
 // Calling function currentDateAndYear to populate document with info
 
 let monthAndYear = document.querySelector("#month-and-year");
@@ -74,18 +77,17 @@ monthAndYear.innerHTML = currentDateAndYear();
 
 function getWeatherConditions(response) {
   console.log(response.data);
-  document.querySelector("#city-name").innerHTML = response.data.name;
+  document.querySelector("#city-name2").innerHTML = response.data.name;
   document.querySelector("#current-temperature").innerHTML = `${Math.round(
     response.data.main.temp
   )}°`;
-  document.querySelector("#same-day-max").innerHTML = `${Math.round(
-    response.data.main.temp_max
-  )}°`;
+  //this uses the same weather API to populate the icon associated to "current weather"
+  document.querySelector("#icon").setAttribute("src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+  
+  document.querySelector("#weather-description").innerHTML = response.data.weather[0].main;
   document.querySelector("#feels-like").innerHTML = `Feels like ${Math.round(
     response.data.main.feels_like
-  )}°`;
-  document.querySelector("#same-day-min").innerHTML = `${Math.round(
-    response.data.main.temp_min
   )}°`;
   document.querySelector(
     "#humidity"
@@ -97,18 +99,75 @@ function getWeatherConditions(response) {
   celsiusTemp = response.data.main.temp;
 }
 
-//This gets the city typed by the user and input that into the document
+//this function helps to format the time presented with the forecast info
+
+function formatHours(timestamp) {
+  let now = new Date(timestamp);
+    let hh = now.getHours();
+  if (hh < 10) {
+    hh = `0${hh}`;
+  }
+  let min = now.getMinutes();
+  if (min < 10) {
+    min = `0${min}`;
+  }
+  return `${hh}:${min}`;
+}
+
+//This function get the forecast from the forecast API and presents it into the HTML
+function getForecast(response) {
+  console.log(response);
+  let forecastElement = document.querySelector("#weather-forecast");
+  forecastElement.innerHTML = null;
+  let forecast = null; // this is to clean up the document with each search 
+
+  for (let index = 0; index < 6; index++) { //this loops until the defined item in a array
+  
+    forecast = response.data.list[index];
+    
+    //Here JS is injecting info into the document. 
+    forecastElement.innerHTML += //the += tells JS to aggregates the info from the array to the prior one
+      `<div class="col">
+          <div class="card text-center"> 
+            <div class="card-body bg-transparent">
+              <h5 class="card-title"> ${formatHours(forecast.dt * 1000)}</h5>
+              <img src="http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png" width="50px"/>
+
+              <div class="row">
+                <div id="same-day" class="col">
+                  <h5 id="same-day-min" class="card-title p-1">${Math.round(forecast.main.temp_min)}°</h5>
+                </div>
+                <div class="col">
+                  <h5 id="same-day-max" class="card-title p-1">${Math.round(forecast.main.temp_max)}°</h5>
+                </div>
+              </div>
+              
+            </div>
+          </div>`
+  }
+}
+
+//This gets the city typed by the user and inputs that into the document
 // Also assign the city into a variable that is added to the apiUrl
 // This then call the above function to ask it to get the temperature and present it into the document
 
 function search(city) {
   let unit = `metric`;
   let apiKey = `271356899704dafebcd929dfba41015b`;
-  let apiEndPoint = `https://api.openweathermap.org/data/2.5/weather`;
-  let apiUrl = `${apiEndPoint}?q=${city}&appid=${apiKey}&units=${unit}`;
+  let apiEndPointWeather = `https://api.openweathermap.org/data/2.5/weather`;
+  let apiUrlWeather = `${apiEndPointWeather}?q=${city}&appid=${apiKey}&units=${unit}`;
 
-  axios.get(apiUrl).then(getWeatherConditions);
+  let apiEndPointForecast = `https://api.openweathermap.org/data/2.5/forecast`;
+  let apiUrlForecast = `${apiEndPointForecast}?q=${city}&appid=${apiKey}&units=${unit}`;
+
+  axios.get(apiUrlWeather).then(getWeatherConditions);
+
+  
+  axios.get(apiUrlForecast).then(getForecast);
+
 }
+
+//This function serve as bridge between the action of the user of typing something and the ability of getting the value from the API
 
 function handleSubmit(event) {
   event.preventDefault();
@@ -121,30 +180,46 @@ function handleSubmit(event) {
 let searchBar = document.querySelector("#search-bar"); //box that is filled
 searchBar.addEventListener("submit", handleSubmit);
 
+//This function get the weather/forecast info based on the user coordenates if the button is clicked
+
 function getMyLocationButton(position) {
   console.log(position);
   let lon = position.coords.longitude;
   let lat = position.coords.latitude;
   let unit = `metric`;
   let apiKey = `271356899704dafebcd929dfba41015b`;
-  let apiEndPoint = `https://api.openweathermap.org/data/2.5/weather`;
-  let apiUrl = `${apiEndPoint}?units=${unit}&lat=${lat}&lon=${lon}&appid=${apiKey}`;
-  axios.get(apiUrl).then(getWeatherConditions);
+  let apiEndPointWeather = `https://api.openweathermap.org/data/2.5/weather`;
+  let apiUrlWeather = `${apiEndPointWeather}?units=${unit}&lat=${lat}&lon=${lon}&appid=${apiKey}`;
+
+  let apiEndPointForecast = `https://api.openweathermap.org/data/2.5/forecast`;
+  let apiUrlForecast = `${apiEndPointForecast}?units=${unit}&lat=${lat}&lon=${lon}&appid=${apiKey}`;
+
+  axios.get(apiUrlWeather).then(getWeatherConditions);
+
+  axios.get(apiUrlForecast).then(getForecast);
+
+
 }
+//This function tells JS to get the user's coordenates
 
 function callNavigator(response) {
   navigator.geolocation.getCurrentPosition(getMyLocationButton);
 }
+//this tells JS to get the user coordenates only if the button is clicked
 
 let zipCodeButton = document.querySelector("#zip-code-button1");
 zipCodeButton.addEventListener("click", callNavigator);
 
+//This is a global variable. It goes outside of a function, so others functions can use it
 let celsiusTemp = null;
+
 
 function displayFahrenheitTemp(event) {
   event.preventDefault();
+  
   document.querySelector("#current-temperature").innerHTML = `${Math.round(
     celsiusTemp * 1.8 + 32)}°`;
+
 }
 
 
@@ -155,6 +230,7 @@ fahrenheit.addEventListener("click", displayFahrenheitTemp);
 
 function displayCelsiusTemp(event) {
   event.preventDefault();
+
   document.querySelector("#current-temperature").innerHTML = `${Math.round(
     celsiusTemp)}°`;
 }
@@ -166,5 +242,5 @@ celsius.addEventListener("click", displayCelsiusTemp);
 
 
 
-search("Antofagasta");
+search("Santiago");
 
